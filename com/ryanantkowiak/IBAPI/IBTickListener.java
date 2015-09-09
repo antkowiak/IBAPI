dce98f35e2c80de6bcdd231bf0db1579a1fa7b3a
Ryan Antkowiak <antkowiak@gmail.com>
2015-09-09 18:00:51 -0500

    Create IBTickListener.java

---
+++ com/ryanantkowiak/IBAPI/IBTickListener.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.ryanantkowiak.IBAPI;
+
+/**
+ * @author Ryan Antkowiak (antkowiak@gmail.com)
+ *
+ */
+public interface IBTickListener
+{
+
+	public abstract void tickPrice(int tickerId, int field, double price, int canAutoExecute);
+
+	public abstract void tickSize(int tickerId, int field, int size);
+
+	public abstract void tickOptionComputation(	int tickerId,
+												int field,
+												double impliedVol,
+												double delta,
+												double optPrice,
+												double pvDividend,
+												double gamma,
+												double vega,
+												double theta,
+												double undPrice);
+
+	public abstract void tickGeneric(int tickerId, int tickType, double value);
+
+	void tickString(int tickerId, int tickType, String value);
+
+	public abstract void tickEFP(	int tickerId,
+									int tickType,
+									double basisPoints,
+									String formattedBasisPoints,
+									double impliedFuture,
+									int holdDays,
+									String futureExpiry,
+									double dividendImpact,
+									double dividendsToExpiry);
+
+}
