7cbf28ee5e951f7ffee1935f81f848d52c2f5ad8
Ryan Antkowiak <antkowiak@gmail.com>
2015-09-09 17:57:56 -0500

    Create IBCommunicationListener.java

---
+++ com/ryanantkowiak/IBAPI/IBCommunicationListener.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.ryanantkowiak.IBAPI;
+
+/**
+ * @author Ryan Antkowiak (antkowiak@gmail.com)
+ *
+ */
+public interface IBCommunicationListener
+{
+	
+	public abstract void handleCommmunicationText(String text);
+	
+}
