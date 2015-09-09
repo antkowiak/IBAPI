68b496d18344033f640345cbbb32b48ac5809dde
Ryan Antkowiak <antkowiak@gmail.com>
2015-09-09 17:58:38 -0500

    Create IBErrorMessageListener.java

---
+++ com/ryanantkowiak/IBAPI/IBErrorMessageListener.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.ryanantkowiak.IBAPI;
+
+/**
+ * @author Ryan Antkowiak (antkowiak@gmail.com)
+ *
+ */
+public interface IBErrorMessageListener
+{
+	public abstract void handleErrorMessage(String text);
+}
