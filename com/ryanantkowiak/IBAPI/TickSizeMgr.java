ab61ecab871dd0c305988c7de20ba19806242b8d
Ryan Antkowiak <antkowiak@gmail.com>
2015-09-09 18:04:27 -0500

    Create TickSizeMgr.java

---
+++ com/ryanantkowiak/IBAPI/TickSizeMgr.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.ryanantkowiak.IBAPI;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ryan Antkowiak (antkowiak@gmail.com)
+ *
+ */
+public class TickSizeMgr
+{
+	private static Map<Integer, TickSizeData> m_tickSizes = new HashMap<Integer, TickSizeData>();
+	
+	public static synchronized void tickSize(int tickerId, int field, int size)
+	{
+		TickSizeData tsd = m_tickSizes.get(tickerId);
+			
+		if (null == tsd)
+		{
+			tsd = new TickSizeData();
+		}
+		
+		tsd.tickSize(tickerId, field, size);
+	}
+	
+	public static synchronized TickSizeData getTickSizeData(int tickerId)
+	{
+		return m_tickSizes.get(tickerId).clone();
+	}
+}
